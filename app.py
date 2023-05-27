2a189d9244a0de9ce3c365fac73980f660b081b5
Priyanshu Sharma <sharmapriyanshu119@gmail.com>
2023-05-27 09:35:43 -0400

    added myenv to .gitignore

--- app.py
+++ app.py
@@ -1,13 +1,95 @@
 from flask import Flask
 from flask import request
+from flask_cors import CORS, cross_origin
+
+
+# import os
+# import pickle
+# from contextlib import nullcontext
+# import torch
+# import tiktoken
+# from model import GPTConfig, GPT
+
+
 
 app = Flask(__name__)
+# cors = CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
 
+out_dir = "/data/discord/out-discord-dir"
+
+
+# needs to be given the start string 
+def helper(start:str, out_dir):
+    init_from = 'resume' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
+    num_samples = 2 # number of samples to draw
+    max_new_tokens = 500 # number of tokens generated in each sample
+    temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
+    top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
+    seed = 1337
+    device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
+    dtype = 'float16' # 'float32' or 'bfloat16' or 'float16'
+    compile = False # use PyTorch 2.0 to compile the model to be faster
+
+
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
+    torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
+    device_type = 'cuda' if 'cuda' in device else 'cpu' # for later use in torch.autocast
+    ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
+    ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
+
+    if init_from == 'resume':
+        # init from a model saved in a specific directory
+        ckpt_path = os.path.join(out_dir, 'ckpt.pt')
+        checkpoint = torch.load(ckpt_path, map_location=device)
+        gptconf = GPTConfig(**checkpoint['model_args'])
+        model = GPT(gptconf)
+        state_dict = checkpoint['model']
+        unwanted_prefix = '_orig_mod.'
+        for k,v in list(state_dict.items()):
+            if k.startswith(unwanted_prefix):
+                state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
+        model.load_state_dict(state_dict)
+    elif init_from.startswith('gpt2'):
+        # init from a given GPT-2 model
+        model = GPT.from_pretrained(init_from, dict(dropout=0.0))
+    
+
+    load_meta = False
+
+    if init_from == 'resume' and 'config' in checkpoint and 'dataset' in checkpoint['config']: # older checkpoints might not have these...
+        meta_path = os.path.join('data', checkpoint['config']['dataset'], 'meta.pkl')
+        load_meta = os.path.exists(meta_path)
+    if load_meta:
+        print(f"Loading meta from {meta_path}...")
+        with open(meta_path, 'rb') as f:
+            meta = pickle.load(f)
+        # TODO want to make this more general to arbitrary encoder/decoder schemes
+        stoi, itos = meta['stoi'], meta['itos']
+        encode = lambda s: [stoi[c] for c in s]
+        decode = lambda l: ''.join([itos[i] for i in l])
+
+    start_ids = encode(start)
+    x = (torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...])
+
+    outputs = []
+    # run generation
+    with torch.no_grad():
+        with ctx:
+            for k in range(num_samples):
+                y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
+                outputs.append(decode(y[0].tolist()))
+    
+    return outputs
+
+    
 @app.route("/", methods=['POST'])
 def home():
     if(request.method == 'POST'):
         data = request.get_json()
-        print(data)
+
     return "hi"
 
 
